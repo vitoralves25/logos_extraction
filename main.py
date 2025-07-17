4fb30d2d4befd42b729bb6acc48378d352b3c970
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-17 15:35:09 -0300

    Atualizando textos

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import fitz
 import re
+
+
 file =  'Marcas2845.pdf'
 open_file = fitz.open(file)
-for pag in open_file:
-    print(pag)
+for page in range(len(dir(open_file))):
+    if page > 10 and page < 12:
+        pagina = open_file.load_page(page)
+        page = pagina.get_images(full=True)
+        print(page)
+
+    #pagina = open_file.load_page(page)
+    #page = pagina.get_images(full=True)
+    #print(page)
