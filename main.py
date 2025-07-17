f5b484e9ae7371c9fad718a3ad5c1afa098231d4
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-17 17:44:13 -0300

    Atualizando

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import fitz
 import re
 
-
+#Selecionei o caminho do arquivo
 file =  'Marcas2845.pdf'
+
+#Abri o arquivo
 open_file = fitz.open(file)
-for page in range(len(dir(open_file))):
+
+
+
+#Pegar cada página do arquivo e cada imagem dele
+for page in range(len(dir(open_file))): 
     if page > 10 and page < 12:
         pagina = open_file.load_page(page)
         page = pagina.get_images(full=True)
 
-        #for i in page:
-    open_file.extract_image
+        text = pagina.get_text()
+        print(text)
+        print(page)
+        for pos, i in enumerate(page):
+            print(f'Imagem_{pos}.png')
+
+
     
