71251d2c6036690fd0ebd780499e4656f897b4ea
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-17 16:12:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ for page in range(len(dir(open_file))):
     if page > 10 and page < 12:
         pagina = open_file.load_page(page)
         page = pagina.get_images(full=True)
-        print(page)
 
-        for i in page:
-            print(i)
+        #for i in page:
+    open_file.extract_image
     
